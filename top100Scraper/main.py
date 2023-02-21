781c03861e61f1a6dc683e5f385de761885a4266
Rohit Nair <87155954+rohitNair21@users.noreply.github.com>
2023-02-21 14:13:38 -0700

    Updated the loading functionalities to thread.

--- top100Scraper/main.py
+++ top100Scraper/main.py
@@ -1,5 +1,6 @@
 import customtkinter
 import tkinter
+from tkinter import filedialog
 import threading
 from time import sleep
 from billTop100Scrape import scrapeAndMake
@@ -8,6 +9,7 @@ import os
 myPath = os.path.dirname(os.path.realpath(__file__))
 imageA = customtkinter.CTkImage(Image.open(myPath +"/logo-internet-clipart-internet-logo-clipground-33.jpeg"), size=(25,25))
 imageB = customtkinter.CTkImage(Image.open(myPath +"/file-spotify-logo-png-4.png"), size=(25,25))
+imageC = customtkinter.CTkImage(Image.open(myPath +"/image_processing20200424-23346-xarvu8.png"), size=(25,25))
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -28,9 +30,20 @@ class App(customtkinter.CTk):
         fromWeb = customtkinter.CTkButton(master=frame, text="Import From Web...", command=self.loading, width=100, height=50, image=imageA)
         fromWeb.pack(pady=15,padx=15)
 
+        fromFile = customtkinter.CTkButton(master=frame, text="Import From File...", command=self.chooseFile, width=100, height=50, image=imageC)
+        fromFile.pack(pady=15,padx=15)
+    
+    def chooseFile(self):
+        filePath = filedialog.askopenfilename()
+        self.loadingFile(filePath)
+
     def generatePlaylist(self):
         playlist = scrapeAndMake()
         playlist.addSongs()
+
+    def generatePlaylistFromFile(self,fileName):
+        playlist = scrapeAndMake()
+        playlist.addSongsFromFile(fileName)
 
     def loading(self):
         loadScreen = customtkinter.CTk()
@@ -40,7 +53,40 @@ class App(customtkinter.CTk):
         frame.pack(pady=20,padx=60,fill="both",expand=True)
         text = customtkinter.CTkLabel(master=frame, text="Creating playlist.", font=("Roboto", 30))
         text.pack(pady=15, padx=15)
-        thread = threading.Thread(target=self.generatePlaylist)
+        thread = threading.Thread(target=self.generatePlaylist())
+        thread.daemon = True
+        thread.start()
+        x = 0
+        loadScreen.update_idletasks()
+        while thread.is_alive():
+            sleep(0.5)
+            if x == 0:
+                loadScreen.update()
+                text.configure(text="Creating playlist.")
+                loadScreen.update()
+                x += 1
+            elif x == 1:
+                loadScreen.update()
+                text.configure(text="Creating playlist..")
+                loadScreen.update()
+                x += 1
+            elif x == 2:
+                loadScreen.update()
+                text.configure(text="Creating playlist...")
+                loadScreen.update()
+                x = 0
+        loadScreen.destroy()
+
+
+    def loadingFile(self, filePath):
+        loadScreen = customtkinter.CTk()
+        loadScreen.geometry("250,250")
+        loadScreen.title("")
+        frame = customtkinter.CTkFrame(master=loadScreen)
+        frame.pack(pady=20,padx=60,fill="both",expand=True)
+        text = customtkinter.CTkLabel(master=frame, text="Creating playlist.", font=("Roboto", 30))
+        text.pack(pady=15, padx=15)
+        thread = threading.Thread(target=self.generatePlaylistFromFile(filePath))
         thread.daemon = True
         thread.start()
         x = 0
